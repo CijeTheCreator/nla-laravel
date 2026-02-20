d4de89769d7363bd6369bab0065eb96edd152837
Chijioke Osadebe <101515459+CijeTheCreator@users.noreply.github.com>
2026-02-20 09:21:05 +0100

    Added route names

--- routes/web.php
+++ routes/web.php
@@ -16,35 +16,42 @@ Route::get('/', function () {
     return view('home');
 })->name('home');
 
-Route::get('/archive', Archive::class);
-Route::get('/archive/{volumeId}', Volume::class);
-Route::get('/archive/{volumeId}/{articleId}', Article::class);
-Route::get('/editorial-team', EditorialTeam::class);
-Route::get('/contact', Contact::class);
-Route::get('/note-to-authors', NotesToAuthors::class);
+Route::get('/archive', Archive::class)->name('archive.index');
+Route::get('/archive/{volumeId}', Volume::class)->name('archive.volume');
+Route::get('/archive/{volumeId}/{articleId}', Article::class)->name('archive.article');
 
-Route::middleware('auth')->prefix('admin')->group(function () {
-    Route::prefix('archive')->group(function () {
-        Route::get('/create', [VolumeController::class, 'create']);
-        Route::post('/create', [VolumeController::class, 'store']);
-        Route::get('/{volume}/edit', [VolumeController::class, 'edit']);
-        Route::put('/{volume}/edit', [VolumeController::class, 'update']);
-        Route::delete('/{volume}/delete', [VolumeController::class, 'destroy']);
+Route::get('/editorial-team', EditorialTeam::class)->name('editorial.team');
+Route::get('/contact', Contact::class)->name('contact');
+Route::get('/note-to-authors', NotesToAuthors::class)->name('notes.authors');
+
+Route::middleware('auth')
+    ->prefix('admin')
+    ->name('admin.')
+    ->group(function () {
+
+        Route::prefix('archive')
+            ->name('archive.')
+            ->group(function () {
+                Route::get('/create', [VolumeController::class, 'create'])->name('create');
+                Route::post('/create', [VolumeController::class, 'store'])->name('store');
+                Route::get('/{volume}/edit', [VolumeController::class, 'edit'])->name('edit');
+                Route::put('/{volume}/edit', [VolumeController::class, 'update'])->name('update');
+                Route::delete('/{volume}/delete', [VolumeController::class, 'destroy'])->name('destroy');
+            });
+
+        Route::prefix('article')
+            ->name('article.')
+            ->group(function () {
+                Route::get('/create', [ArticleController::class, 'create'])->name('create');
+                Route::post('/create', [ArticleController::class, 'store'])->name('store');
+                Route::get('/{article}/edit', [ArticleController::class, 'edit'])->name('edit');
+                Route::put('/{article}/edit', [ArticleController::class, 'update'])->name('update');
+                Route::delete('/{article}/delete', [ArticleController::class, 'destroy'])->name('destroy');
+            });
     });
 
-    Route::prefix('article')->group(function () {
-        Route::get('/create', [ArticleController::class, 'create']);
-        Route::post('/create', [ArticleController::class, 'store']);
-        Route::get('/{article}/edit', [ArticleController::class, 'edit']);
-        Route::put('/{article}/edit', [ArticleController::class, 'update']);
-        Route::delete('/{article}/delete', [ArticleController::class, 'destroy']);
-    });
-});
-
-Route::view('/login', 'login')
-    ->name('login');
-
-Route::post('/login', Login::class);
-
-// Logout route
-Route::post('/logout', Logout::class)->middleware('auth');
+Route::view('/login', 'login')->name('login');
+Route::post('/login', Login::class)->name('login.attempt');
+Route::post('/logout', Logout::class)
+    ->middleware('auth')
+    ->name('logout');
